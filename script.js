2126f92a9cddfebb4318843505cf2ae6d9361058
ericbuys <eric.buys02@gmail.com>
2022-06-04 09:56:25 -0400

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return Math.round((a + b) * 100)/100;
+}
+
+function subtract(a, b) {
+    return Math.round((a - b) * 100)/100;
+}
+
+function multiply(a, b) {
+    return Math.round((a * b) * 100)/100;
+}
+
+function divide(a, b) {
+    return Math.round((a / b) * 100)/100;
+}
